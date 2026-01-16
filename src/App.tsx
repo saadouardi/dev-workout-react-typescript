3396d7b6ea09932542b8e42ab0ce9b12d0552064
SaadOuardi <saad.ouardi.developer@gmail.com>
2026-01-16 03:27:30 +0100

    adding @ as path alias

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom"
-import { CartProvider } from "./context/CartContext"
-import { Header } from "./components/Header";
-import { Footer } from "./components/Footer";
-import { ProductList } from "./pages/ProductList";
-import { ProductDetail  } from "./pages/ProductDetail";
+import { CartProvider } from "@/context/CartContext"
+import { Header } from "@/components/Header";
+import { Footer } from "@/components/Footer";
+import { ProductList } from "@/pages/ProductList";
+import { ProductDetail  } from "@/pages/ProductDetail";
 
 export default function App() {
   return (
